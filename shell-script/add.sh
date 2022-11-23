f4574023c211e58f845ebd8d8b6b43db577a18ba
fishbone440 <ayubailiyaj@gmail.com>
2022-11-23 09:17:38 +0000

    second commit

---
+++ shell-script/add.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+
+# Add two numbers
+a=$1
+b=$2
+((sum=a+b))
+
+echo $sum
+
---
+++ shell-script/ayuba.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "what is your name?"
+read name
+echo "How do you do, $name?"
+read remark
+echo "I am $remark too!"
+echo "How can i help you today? $name"
+read remark
+echo "wait a minute $name"
+read remarks
+echo "THANKS $name"
